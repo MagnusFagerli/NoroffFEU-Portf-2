60260d23ac2e5ed0343cc70abdf80ca0dbedb8c4
Magnus Fagerli <magnusfli@gmail.com>
2022-06-07 10:22:36 +0200

    update
    remove filler images

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,26 +1,26 @@
 import React from "react";
 import Cards from "../portfolio/cards/Cards";
 import Header from "../headers/Header";
-import iphoneView from "../../images/iphoneView.png";
-import selfie from "../../images/selfie.png";
+import IphoneView from "../../images/iphoneView.png";
+import Selfie from "../../images/selfie.png";
 
 function Home() {
   return (
     <>
       <div className="homeIntro">
-        <Header>Test</Header>
+        <Header>Hello!</Header>
       </div>
       <div className="homeCards">
         <Cards
           header="Hello, I'm Magnus!"
-          image={selfie}
+          image={Selfie}
           p="Front End Developer from Oslo, Norway. Currently working at Norges Automobil Forbund (NAF) as a Front End Developer.
-        I'm passionate about creating beautiful and functional websites and applications."
+          I'm passionate about creating beautiful and functional websites and applications."
           href="https://github.com/MagnusFagerli"
         />
         <Cards
           header="Currently working on..."
-          image={iphoneView}
+          image={IphoneView}
           p="Currently working on our Membership App at NAF, which is created with React Native and Typescript."
           href="https://medlemsapp.naf.no/"
         />
--- src/components/portfolio/Projects.js
+++ src/components/portfolio/Projects.js
@@ -2,8 +2,9 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 import Header from "../headers/Header";
 import JSCA from "../../images/jscaimage.png";
+import MintSneaks from "../../images/mintsneaksimage.png";
+import Holidaze from "../../images/holidazeimage.png";
 import Cards from "./cards/Cards";
-import Car from "../../images/test.jpeg";
 
 function Projects() {
   return (
@@ -28,19 +29,19 @@ function Projects() {
       </div>
       <Cards
         header="MintSneaks"
-        image={Car}
+        image={MintSneaks}
         alt="Car"
         p={
           "Semester Project 2. E-commerce site with Strapi CMS. Predefined products and admin login provided by Noroff."
         }
-        href="https://www.vg.no"
+        href="https://github.com/MagnusFagerli/MintSneaks"
       />
       <div className="linkButton">
         <Button variant="disabled">View Live</Button>
       </div>
       <Cards
         header="Holidaze"
-        image={Car}
+        image={Holidaze}
         alt="car"
         p={
           "Project Exam of 2022. Hotel booking website with JWT tokens for admin login, add/edit accommodations for the WordPress CMS."
@@ -50,7 +51,7 @@ function Projects() {
       <div className="linkButton">
         <Button
           variant="primary"
-          href="https://www.tek.no"
+          href="https://splendorous-bubblegum-75fb71.netlify.app/"
           target="_blank"
           rel="noreferrer"
         >
