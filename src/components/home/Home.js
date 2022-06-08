246025db0ff2eeb7ddce0556906787577726fc0d
Magnus Fagerli <magnusfli@gmail.com>
2022-06-08 08:27:50 +0200

    update
    layout to homepage

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,17 +1,14 @@
 import React from "react";
 import Cards from "../portfolio/cards/Cards";
-import Header from "../headers/Header";
 import IphoneView from "../../images/iphoneView.png";
 import Selfie from "../../images/selfie.png";
 
 function Home() {
   return (
     <>
-      <div className="homeIntro">
-        <Header>Hello!</Header>
-      </div>
-      <div className="homeCards">
+      <div className="homeCardsContainer">
         <Cards
+          className="homeCard"
           header="Hello, I'm Magnus!"
           image={Selfie}
           p="Front End Developer from Oslo, Norway. Currently working at Norges Automobil Forbund (NAF) as a Front End Developer.
@@ -19,6 +16,7 @@ function Home() {
           href="https://github.com/MagnusFagerli"
         />
         <Cards
+          className="homeCard"
           header="Currently working on..."
           image={IphoneView}
           p="Currently working on our Membership App at NAF, which is created with React Native and Typescript."
